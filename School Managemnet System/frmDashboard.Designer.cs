241524814142508c168fa685e8969cf187a62142
Ahmedkhn143 <ahmadkha8143@gmail.com>
2026-04-25 22:42:34 +0500

    Fix frmStudentRegistration Designer: Correct TextBox vs Label controls for proper input handling

--- School Managemnet System/frmDashboard.Designer.cs
+++ School Managemnet System/frmDashboard.Designer.cs
@@ -39,18 +39,22 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(317, 37);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(507, 25);
             this.label2.TabIndex = 0;
-            this.label2.Text = "label2";
+            this.label2.Text = "School Management System - Main Dashboard";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // btnStudent
             // 
-            this.btnStudent.Location = new System.Drawing.Point(29, 44);
+            this.btnStudent.Font = new System.Drawing.Font("Sylfaen", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStudent.Location = new System.Drawing.Point(13, 115);
+            this.btnStudent.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnStudent.Name = "btnStudent";
-            this.btnStudent.Size = new System.Drawing.Size(75, 23);
+            this.btnStudent.Size = new System.Drawing.Size(112, 61);
             this.btnStudent.TabIndex = 1;
             this.btnStudent.Text = "Student";
             this.btnStudent.UseVisualStyleBackColor = true;
@@ -58,36 +62,44 @@
             // 
             // btnAttendance
             // 
-            this.btnAttendance.Location = new System.Drawing.Point(29, 83);
+            this.btnAttendance.Font = new System.Drawing.Font("Sylfaen", 10F, System.Drawing.FontStyle.Bold);
+            this.btnAttendance.Location = new System.Drawing.Point(13, 186);
+            this.btnAttendance.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnAttendance.Name = "btnAttendance";
-            this.btnAttendance.Size = new System.Drawing.Size(75, 23);
+            this.btnAttendance.Size = new System.Drawing.Size(133, 61);
             this.btnAttendance.TabIndex = 2;
             this.btnAttendance.Text = "Attendance";
             this.btnAttendance.UseVisualStyleBackColor = true;
             // 
             // btnFee
             // 
-            this.btnFee.Location = new System.Drawing.Point(29, 122);
+            this.btnFee.Font = new System.Drawing.Font("Sylfaen", 10F, System.Drawing.FontStyle.Bold);
+            this.btnFee.Location = new System.Drawing.Point(13, 257);
+            this.btnFee.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnFee.Name = "btnFee";
-            this.btnFee.Size = new System.Drawing.Size(75, 23);
+            this.btnFee.Size = new System.Drawing.Size(112, 64);
             this.btnFee.TabIndex = 3;
             this.btnFee.Text = "Fee";
             this.btnFee.UseVisualStyleBackColor = true;
             // 
             // btnStaff
             // 
-            this.btnStaff.Location = new System.Drawing.Point(29, 161);
+            this.btnStaff.Font = new System.Drawing.Font("Sylfaen", 10F, System.Drawing.FontStyle.Bold);
+            this.btnStaff.Location = new System.Drawing.Point(13, 346);
+            this.btnStaff.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnStaff.Name = "btnStaff";
-            this.btnStaff.Size = new System.Drawing.Size(75, 23);
+            this.btnStaff.Size = new System.Drawing.Size(112, 66);
             this.btnStaff.TabIndex = 4;
             this.btnStaff.Text = "Staff";
             this.btnStaff.UseVisualStyleBackColor = true;
             // 
             // btnLogout
             // 
-            this.btnLogout.Location = new System.Drawing.Point(29, 200);
+            this.btnLogout.Font = new System.Drawing.Font("Sylfaen", 10F, System.Drawing.FontStyle.Bold);
+            this.btnLogout.Location = new System.Drawing.Point(13, 450);
+            this.btnLogout.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(75, 23);
+            this.btnLogout.Size = new System.Drawing.Size(112, 71);
             this.btnLogout.TabIndex = 5;
             this.btnLogout.Text = "Logout";
             this.btnLogout.UseVisualStyleBackColor = true;
@@ -95,15 +107,16 @@
             // 
             // frmDashboard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(894, 465);
+            this.ClientSize = new System.Drawing.Size(1228, 615);
             this.Controls.Add(this.btnLogout);
             this.Controls.Add(this.btnStaff);
             this.Controls.Add(this.btnFee);
             this.Controls.Add(this.btnAttendance);
             this.Controls.Add(this.btnStudent);
             this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmDashboard";
             this.Text = "Dashboard";
             this.Load += new System.EventHandler(this.frmDashboard_Load);
--- School Managemnet System/frmStudentRegistration.Designer.cs
+++ School Managemnet System/frmStudentRegistration.Designer.cs
@@ -29,17 +29,102 @@
         private void InitializeComponent()
         {
             this.btnSave = new System.Windows.Forms.Button();
-            this.txtPassword = new System.Windows.Forms.TextBox();
-            this.txtEmail = new System.Windows.Forms.TextBox();
-            this.txtFullName = new System.Windows.Forms.Label();
-            this.txtRegNo = new System.Windows.Forms.Label();
-            this.txtFatherName = new System.Windows.Forms.Label();
-            this.txtContactNo = new System.Windows.Forms.Label();
-            this.txtClassName = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtRegNo = new System.Windows.Forms.TextBox();
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.txtFatherName = new System.Windows.Forms.TextBox();
+            this.txtClassName = new System.Windows.Forms.TextBox();
+            this.txtContactNo = new System.Windows.Forms.TextBox();
+            this.lblRegNo = new System.Windows.Forms.Label();
+            this.lblFullName = new System.Windows.Forms.Label();
+            this.lblFatherName = new System.Windows.Forms.Label();
+            this.lblClassName = new System.Windows.Forms.Label();
+            this.lblContactNo = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblRegNo
+            // 
+            this.lblRegNo.AutoSize = true;
+            this.lblRegNo.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegNo.Location = new System.Drawing.Point(84, 72);
+            this.lblRegNo.Name = "lblRegNo";
+            this.lblRegNo.Size = new System.Drawing.Size(177, 27);
+            this.lblRegNo.TabIndex = 0;
+            this.lblRegNo.Text = "Registration No:";
+            // 
+            // txtRegNo
+            // 
+            this.txtRegNo.Location = new System.Drawing.Point(163, 74);
+            this.txtRegNo.Name = "txtRegNo";
+            this.txtRegNo.Size = new System.Drawing.Size(187, 26);
+            this.txtRegNo.TabIndex = 1;
+            // 
+            // lblFullName
+            // 
+            this.lblFullName.AutoSize = true;
+            this.lblFullName.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFullName.Location = new System.Drawing.Point(371, 73);
+            this.lblFullName.Name = "lblFullName";
+            this.lblFullName.Size = new System.Drawing.Size(121, 27);
+            this.lblFullName.TabIndex = 2;
+            this.lblFullName.Text = "Full Name:";
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.Location = new System.Drawing.Point(503, 75);
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.Size = new System.Drawing.Size(238, 26);
+            this.txtFullName.TabIndex = 3;
+            // 
+            // lblFatherName
+            // 
+            this.lblFatherName.AutoSize = true;
+            this.lblFatherName.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFatherName.Location = new System.Drawing.Point(84, 144);
+            this.lblFatherName.Name = "lblFatherName";
+            this.lblFatherName.Size = new System.Drawing.Size(149, 27);
+            this.lblFatherName.TabIndex = 4;
+            this.lblFatherName.Text = "Father Name:";
+            // 
+            // txtFatherName
+            // 
+            this.txtFatherName.Location = new System.Drawing.Point(163, 146);
+            this.txtFatherName.Name = "txtFatherName";
+            this.txtFatherName.Size = new System.Drawing.Size(187, 26);
+            this.txtFatherName.TabIndex = 5;
+            // 
+            // lblClassName
+            // 
+            this.lblClassName.AutoSize = true;
+            this.lblClassName.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClassName.Location = new System.Drawing.Point(371, 144);
+            this.lblClassName.Name = "lblClassName";
+            this.lblClassName.Size = new System.Drawing.Size(126, 27);
+            this.lblClassName.TabIndex = 6;
+            this.lblClassName.Text = "Class Name:";
+            // 
+            // txtClassName
+            // 
+            this.txtClassName.Location = new System.Drawing.Point(503, 146);
+            this.txtClassName.Name = "txtClassName";
+            this.txtClassName.Size = new System.Drawing.Size(238, 26);
+            this.txtClassName.TabIndex = 7;
+            // 
+            // lblContactNo
+            // 
+            this.lblContactNo.AutoSize = true;
+            this.lblContactNo.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContactNo.Location = new System.Drawing.Point(84, 215);
+            this.lblContactNo.Name = "lblContactNo";
+            this.lblContactNo.Size = new System.Drawing.Size(149, 27);
+            this.lblContactNo.TabIndex = 8;
+            this.lblContactNo.Text = "Contact No:";
+            // 
+            // txtContactNo
+            // 
+            this.txtContactNo.Location = new System.Drawing.Point(163, 217);
+            this.txtContactNo.Name = "txtContactNo";
+            this.txtContactNo.Size = new System.Drawing.Size(187, 26);
+            this.txtContactNo.TabIndex = 9;
             // 
             // btnSave
             // 
@@ -47,116 +132,29 @@
             this.btnSave.Location = new System.Drawing.Point(281, 290);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(221, 51);
-            this.btnSave.TabIndex = 11;
+            this.btnSave.TabIndex = 10;
             this.btnSave.Text = "Register Student";
             this.btnSave.UseVisualStyleBackColor = true;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
-            // 
-            // txtPassword
-            // 
-            this.txtPassword.Location = new System.Drawing.Point(503, 75);
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(238, 26);
-            this.txtPassword.TabIndex = 10;
-            // 
-            // txtEmail
-            // 
-            this.txtEmail.Location = new System.Drawing.Point(163, 74);
-            this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(187, 26);
-            this.txtEmail.TabIndex = 9;
-            // 
-            // txtFullName
-            // 
-            this.txtFullName.AutoSize = true;
-            this.txtFullName.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtFullName.Location = new System.Drawing.Point(371, 73);
-            this.txtFullName.Name = "txtFullName";
-            this.txtFullName.Size = new System.Drawing.Size(121, 27);
-            this.txtFullName.TabIndex = 8;
-            this.txtFullName.Text = "Full Name:";
-            // 
-            // txtRegNo
-            // 
-            this.txtRegNo.AutoSize = true;
-            this.txtRegNo.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtRegNo.Location = new System.Drawing.Point(84, 72);
-            this.txtRegNo.Name = "txtRegNo";
-            this.txtRegNo.Size = new System.Drawing.Size(177, 27);
-            this.txtRegNo.TabIndex = 7;
-            this.txtRegNo.Text = "Registration No:";
-            this.txtRegNo.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // txtFatherName
-            // 
-            this.txtFatherName.AutoSize = true;
-            this.txtFatherName.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtFatherName.Location = new System.Drawing.Point(84, 144);
-            this.txtFatherName.Name = "txtFatherName";
-            this.txtFatherName.Size = new System.Drawing.Size(149, 27);
-            this.txtFatherName.TabIndex = 13;
-            this.txtFatherName.Text = "Father Name:";
-            // 
-            // txtContactNo
-            // 
-            this.txtContactNo.AutoSize = true;
-            this.txtContactNo.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtContactNo.Location = new System.Drawing.Point(158, 215);
-            this.txtContactNo.Name = "txtContactNo";
-            this.txtContactNo.Size = new System.Drawing.Size(131, 27);
-            this.txtContactNo.TabIndex = 14;
-            this.txtContactNo.Text = "Contact No:";
-            // 
-            // txtClassName
-            // 
-            this.txtClassName.AutoSize = true;
-            this.txtClassName.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtClassName.Location = new System.Drawing.Point(381, 145);
-            this.txtClassName.Name = "txtClassName";
-            this.txtClassName.Size = new System.Drawing.Size(69, 27);
-            this.txtClassName.TabIndex = 15;
-            this.txtClassName.Text = "Class:";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(204, 145);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(162, 26);
-            this.textBox1.TabIndex = 16;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(503, 144);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(238, 26);
-            this.textBox2.TabIndex = 17;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(295, 217);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(238, 26);
-            this.textBox3.TabIndex = 18;
             // 
             // frmStudentRegistration
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.txtClassName);
-            this.Controls.Add(this.txtContactNo);
-            this.Controls.Add(this.txtFatherName);
             this.Controls.Add(this.btnSave);
-            this.Controls.Add(this.txtPassword);
-            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtContactNo);
+            this.Controls.Add(this.lblContactNo);
+            this.Controls.Add(this.txtClassName);
+            this.Controls.Add(this.lblClassName);
+            this.Controls.Add(this.txtFatherName);
+            this.Controls.Add(this.lblFatherName);
             this.Controls.Add(this.txtFullName);
+            this.Controls.Add(this.lblFullName);
             this.Controls.Add(this.txtRegNo);
+            this.Controls.Add(this.lblRegNo);
             this.Name = "frmStudentRegistration";
-            this.Text = "Form1";
+            this.Text = "Student Registration";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -164,15 +162,15 @@
 
         #endregion
         private System.Windows.Forms.Button btnSave;
-        private System.Windows.Forms.TextBox txtPassword;
-        private System.Windows.Forms.TextBox txtEmail;
-        private System.Windows.Forms.Label txtFullName;
-        private System.Windows.Forms.Label txtRegNo;
-        private System.Windows.Forms.Label txtFatherName;
-        private System.Windows.Forms.Label txtContactNo;
-        private System.Windows.Forms.Label txtClassName;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtRegNo;
+        private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.TextBox txtFatherName;
+        private System.Windows.Forms.TextBox txtClassName;
+        private System.Windows.Forms.TextBox txtContactNo;
+        private System.Windows.Forms.Label lblRegNo;
+        private System.Windows.Forms.Label lblFullName;
+        private System.Windows.Forms.Label lblFatherName;
+        private System.Windows.Forms.Label lblClassName;
+        private System.Windows.Forms.Label lblContactNo;
     }
 }
